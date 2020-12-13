45dcbdf117f47e45a0c2b0021b540b11b6068293
Humanskin <807492916@qq.com>
2020-12-14 00:49:21 +0800

    bugfix

--- main.go
+++ main.go
@@ -33,14 +33,15 @@ func main() {
 	defer zap.L().Sync()
 	zap.L().Debug("logger init success...")
 	// 3. 初始化Mysql连接
+	fmt.Println("mysql...")
 	if err := mysql.Init(); err != nil {
-		fmt.Printf("Init logger error: %s\n", err)
+		fmt.Printf("Init Mysql error: %s\n", err)
 		return
 	}
 	defer mysql.Close()
 	// 4. 初始化Redis连接
 	if err := redis.Init(); err != nil {
-		fmt.Printf("Init logger error: %s\n", err)
+		fmt.Printf("Init Redis error: %s\n", err)
 		return
 	}
 	redis.Close()
@@ -48,13 +49,13 @@ func main() {
 	r := routes.Setup()
 	// 6. 启动服务（优雅关机）
 	srv := http.Server{
-		Addr:    fmt.Sprintf("%d", viper.GetInt("app.port")),
+		Addr:    fmt.Sprintf(":%d", viper.GetInt("app.port")),
 		Handler: r,
 	}
 
 	go func() {
 		//
-		if err := srv.ListenAndServe(); err != nil && err == http.ErrServerClosed {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			zap.L().Fatal("listen: ", zap.Error(err))
 		}
 
@@ -64,7 +65,7 @@ func main() {
 		// kill -2 发送 syscall.SIGINT 信号（如 ctrl+c）
 		// kill -9 发送 syscall.SIGKILL 信号，信号无法被捕获所以无需添加
 		// signal.Notify 把收到的 syscall.SIGTERM 或 syscall.SIGINT 信号转发给quit
-		signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 		<-quit // 阻塞，接受信号后继续执行
 		zap.L().Info("Shutdown Server ...")
 		// 创建一个5s超时的context
